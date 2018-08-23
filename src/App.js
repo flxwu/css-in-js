74374d2e115aeb4fd822b66d594a36ae0f11bb5c
Felix Wu <mail@flxwu.com>
2018-08-23 18:18:52 +0200

    outcapsule regular CSS header

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ import logo from './css-in-js.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">CSS in JS Demo</h1>
-          <h4>(Using plain CSS)</h4>
-        </header>
-        <hr/>
-        <ReactDemo/>
-        <hr/>
-        <StyledComponents/>
-        <hr/>
-        <Radium theme={'primary'}/>
-        <hr/>
-      </div>
-    );
-  }
+	regularCSS() {
+		return (
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">CSS in JS Demo</h1>
+				<h4>(Using plain CSS)</h4>
+			</header>
+		);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.regularCSS()}
+				<hr />
+				<ReactDemo />
+				<hr />
+				<StyledComponents />
+				<hr />
+				<Radium theme={'primary'} />
+				<hr />
+			</div>
+		);
+	}
 }
 
 export default App;
